ee0691a6bb92c88e225d77dfb392f923baf2c3ea
khalil-tabakh <khalil.tabakh@outlook.com>
2025-09-26 22:16:21 +0200

    refactor(scroller): safely expose some properties

--- projects/ngx-uic/scroller/src/components/scroller/scroller.component.ts
+++ projects/ngx-uic/scroller/src/components/scroller/scroller.component.ts
@@ -9,12 +9,12 @@ import { batchAttribute, offsetAttribute } from '../../utils/transforms.util';
     styleUrl: './scroller.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NgxScrollerComponent {
+export class NgxScrollerComponent<Item> {
     private elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
     private injector = inject(Injector);
 
     readonly batch = input(1, { transform: batchAttribute });
-    readonly items = input.required<unknown[]>();
+    readonly items = input.required<Item[]>();
     readonly offset = input(0, { transform: offsetAttribute });
     readonly reverse = input(false, { transform: booleanAttribute });
     readonly threshold = input<number | number[]>();
@@ -23,11 +23,11 @@ export class NgxScrollerComponent {
     readonly first = output<void>();
     readonly last = output<void>();
 
-    protected template = contentChild.required(TemplateRef);
+    protected template = contentChild.required<TemplateRef<{ $implicit: Item }>>(TemplateRef);
 
-    private intersections = signal(new Map<Element, IntersectionObserverEntry>());
+    private entries = signal(new Map<Element, IntersectionObserverEntry>());
 
-    private start = linkedSignal<unknown[], number>({
+    private start = linkedSignal<Item[], number>({
         source: this.items,
         computation: (newItems, previous) => {
             const oldItems = previous?.source ?? newItems;
@@ -36,7 +36,7 @@ export class NgxScrollerComponent {
             return newIndex > -1 ? newIndex : oldIndex;
         }
     });
-    private end = linkedSignal<unknown[], number>({
+    private end = linkedSignal<Item[], number>({
         source: this.items,
         computation: (newItems, previous) => {
             const oldItems = previous?.source ?? newItems;
@@ -46,20 +46,20 @@ export class NgxScrollerComponent {
         }
     });
 
-    protected content = computed(() => this.reverse()
+    readonly content = computed<readonly Item[]>(() => this.reverse()
         ? this.items().slice(this.start(), this.end()).reverse()
         : this.items().slice(this.start(), this.end())
     );
+    readonly intersections = computed<readonly IntersectionObserverEntry[]>(() => this.entries().values().toArray());
 
-    private entries = computed(() => this.intersections().values().toArray());
-    private firstIndex = computed(() => this.entries().findIndex((entry) => entry.isIntersecting));
+    private firstIndex = computed(() => this.intersections().findIndex((intersection) => intersection.isIntersecting));
     private firstOffset = computed(() => {
         const offset = Number(this.offset()) || 0;
         const firstOffset = offset;
         const lastOffset = this.content().length - 1 - offset;
         return firstOffset < lastOffset ? firstOffset : lastOffset > 0 ? lastOffset - 1 : 0;
     });
-    private lastIndex = computed(() => this.entries().findLastIndex((entry) => entry.isIntersecting));
+    private lastIndex = computed(() => this.intersections().findLastIndex((intersection) => intersection.isIntersecting));
     private lastOffset = computed(() => {
         const offset = Number(this.offset()) || 0;
         const lastOffset = this.content().length - 1 - offset;
@@ -70,15 +70,15 @@ export class NgxScrollerComponent {
     private initialized = false;
     private filled = false;
 
-    private intersection$ = new IntersectionObserver((entries) => {
-        if (entries.length === this.content().length) this.intersections.set(new Map(entries.map(entry => [entry.target, entry])));
-        else this.intersections.update((intersections) => new Map(entries.reduce((intersections, entry) => intersections.set(entry.target, entry), intersections)));
+    private intersection$ = new IntersectionObserver((intersections) => {
+        if (intersections.length === this.content().length) this.entries.set(new Map(intersections.map(intersection => [intersection.target, intersection])));
+        else this.entries.update((entries) => new Map(intersections.reduce((entries, intersection) => entries.set(intersection.target, intersection), entries)));
         this.filled ||= this.lastIndex() < this.content().length - 1;
         this.initialized ||= this.firstIndex() > this.firstOffset();
         switch (true) {
             case this.lastIndex() >= this.lastOffset(): {
-                const batch = entries.length === this.content().length ? this.lastIndex() - this.lastOffset() + 1 : this.batch();
-                const virtualize = entries.length === this.content().length ? false : this.virtualize();
+                const batch = intersections.length === this.content().length ? this.lastIndex() - this.lastOffset() + 1 : this.batch();
+                const virtualize = intersections.length === this.content().length ? false : this.virtualize();
                 if (this.end() < this.items().length) {
                     const firstBatch = this.firstIndex() - 1 > this.firstOffset() ? this.firstIndex() - 1 - this.firstOffset() : 0;
                     const lastBatch = this.end() + batch > this.items().length ? this.items().length - this.end() : batch;
@@ -88,8 +88,8 @@ export class NgxScrollerComponent {
                 break;
             }
             case this.firstIndex() <= this.firstOffset(): {
-                const batch = entries.length === this.content().length ? this.firstIndex() - this.firstOffset() - 1 : -this.batch();
-                const virtualize = entries.length === this.content().length ? false : this.virtualize();
+                const batch = intersections.length === this.content().length ? this.firstIndex() - this.firstOffset() - 1 : -this.batch();
+                const virtualize = intersections.length === this.content().length ? false : this.virtualize();
                 if (this.start() > 0) {
                     const firstBatch = this.start() + batch < 0 ? -this.start() : batch;
                     const lastBatch = this.lastIndex() + 1 < this.lastOffset() ? this.lastIndex() + 1 - this.lastOffset() : 0;
@@ -106,9 +106,9 @@ export class NgxScrollerComponent {
 
     private unblockEmitters$ = effect(() => this.emittable = Boolean(this.items()));
     private unshiftContent$ = effect(() => this.content().length && untracked(() => {
-        const entry = this.reverse() ? this.entries().at(-1) : this.entries().at(0);
-        if (!entry?.isIntersecting) return;
-        const child = entry.target as HTMLElement;
+        const intersection = this.reverse() ? this.intersections().at(-1) : this.intersections().at(0);
+        if (!intersection?.isIntersecting) return;
+        const child = intersection.target as HTMLElement;
         const left = this.filled
             ? this.elementRef.nativeElement.scrollLeft - child.offsetLeft
             : this.elementRef.nativeElement.scrollWidth * (this.reverse() ? 1 : -1);
