baad9c6a98ac3237c30088c2b1d82ad39b73a3db
khalil-tabakh <khalil.tabakh@outlook.com>
2026-05-09 21:16:37 +0200

    feat(player): seek by pressing some keyboard buttons

--- projects/ngx-uic/player/src/directives/seek/seek.directive.ts
+++ projects/ngx-uic/player/src/directives/seek/seek.directive.ts
@@ -1,21 +1,31 @@
-import { Directive, inject, input } from '@angular/core';
+import { Directive, ElementRef, afterRenderEffect, inject, input } from '@angular/core';
 import { NgxPlayerComponent } from '../../components/player/player.component';
 
 @Directive({
     selector: '[ngxSeek]',
     host: { 
-        '(click)': 'onSeek($event)',
+        '(click)': '$event.stopPropagation(); onSeek()',
         '(dblclick)': '$event.stopPropagation()'
     },
     exportAs: 'ngxSeek'
 })
 export class NgxSeekDirective {
+    private element = inject<ElementRef<HTMLElement>>(ElementRef).nativeElement;
     private player = inject(NgxPlayerComponent);
 
+    readonly keys = input<string[]>([]);
     readonly value = input.required<number>();
 
-    protected onSeek(event: PointerEvent): void {
-        event.stopPropagation();
+    private toggle$ = afterRenderEffect({
+        read: (onCleanup) => {
+            const player = this.element.closest<HTMLElement>('ngx-player');
+            const onKeydown = (event: KeyboardEvent) => this.keys().includes(event.code) && this.onSeek();
+            player?.addEventListener('keydown', onKeydown);
+            onCleanup(() => player?.removeEventListener('keydown', onKeydown));
+        },
+    });
+
+    protected onSeek(): void {
         const audio = this.player.audio();
         const video = this.player.video();
         const duration = video?.duration ?? audio?.duration ?? 0
