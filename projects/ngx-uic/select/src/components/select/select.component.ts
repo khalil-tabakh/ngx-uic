a75b998efa3e0fb46f2a6d7e9ecfeabe191ae9cf
khalil-tabakh <khalil.tabakh@outlook.com>
2026-06-19 23:37:00 +0200

    feat(select) implement FormValueControl

--- projects/ngx-uic/select/src/components/select/select.component.ts
+++ projects/ngx-uic/select/src/components/select/select.component.ts
@@ -1,5 +1,6 @@
-import { Component, ModelSignal, Signal, booleanAttribute, computed, contentChild, contentChildren, forwardRef, input, model } from '@angular/core';
+import { Component, ModelSignal, Signal, booleanAttribute, computed, contentChild, contentChildren, effect, forwardRef, input, model } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { FormValueControl } from '@angular/forms/signals';
 import { NgxOptionDirective } from '../../directives/option/option.directive';
 import { NgxPopupDirective } from '../../directives/popup/popup.directive';
 import { NgxTriggerDirective } from '../../directives/trigger/trigger.directive';
@@ -16,15 +17,16 @@ import { Multi, Selected, Value } from '../../utils/types.util';
         multi: true
     }]
 })
-export class NgxSelectComponent<M extends boolean | null | number | object | string | undefined, V> implements ControlValueAccessor {
+export class NgxSelectComponent<M extends boolean | null | number | object | string | undefined, V> implements ControlValueAccessor, FormValueControl<Value<M, V>> {
     readonly multi = input(false as Multi<M>, {
         transform: (value: M) => {
-            const result = booleanAttribute(value) as Multi<M>;
-            this.value.set((result ? [] : undefined) as Value<M, V>);
-            return result;
+            const multi = booleanAttribute(value) as Multi<M>;
+            this.reset(multi);
+            return multi;
         }
     });
 
+    readonly disabled = model(false);
     readonly value = model() as ModelSignal<Value<M, V>>;
 
     readonly options = contentChildren(NgxOptionDirective, { descendants: true });
@@ -41,6 +43,15 @@ export class NgxSelectComponent<M extends boolean | null | number | object | str
         }
     }) as Signal<Selected<M>>;
 
+    private toggle$ = effect(() => {
+        const popup = this.popup().element;
+        popup instanceof HTMLDialogElement ? popup.close() : popup.hidePopover();
+        const trigger = this.trigger().element;
+        trigger instanceof HTMLButtonElement || (trigger instanceof HTMLInputElement && trigger.type === 'button')
+            ? trigger.disabled = this.disabled()
+            : trigger.inert = this.disabled();
+    });
+
     /* ControlValueAccessor */
 
     onChange = (_: ReturnType<typeof this.value>): void => {};
@@ -56,13 +67,20 @@ export class NgxSelectComponent<M extends boolean | null | number | object | str
     }
 
     setDisabledState(isDisabled: boolean): void {
-        const popup = this.popup().element;
-        popup instanceof HTMLDialogElement ? popup.close() : popup.hidePopover();
-        const trigger = this.trigger().element;
-        trigger instanceof HTMLButtonElement ? (trigger.disabled = isDisabled) : (trigger.inert = isDisabled);
+        this.disabled.set(isDisabled);
     }
 
     writeValue(value: ReturnType<typeof this.value>): void {
         this.value.set(value);
+    }
+
+    /* FormValueControl */
+
+    focus(options?: FocusOptions): void {
+        this.trigger().element.focus(options);
+    }
+
+    reset(multi = this.multi()): void {
+        this.value.set((multi ? [] : undefined) as ReturnType<typeof this.value>);
     }
 }
