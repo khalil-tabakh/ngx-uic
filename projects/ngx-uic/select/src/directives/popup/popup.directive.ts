b6fa3b4c1f80f00476c8fc2d0dcf0f01f33f5ba2
khalil-tabakh <khalil.tabakh@outlook.com>
2026-06-19 22:03:53 +0200

    feat(select): support any trigger element type

--- projects/ngx-uic/select/src/directives/popup/popup.directive.ts
+++ projects/ngx-uic/select/src/directives/popup/popup.directive.ts
@@ -1,29 +1,39 @@
-import { Directive, ElementRef, inject } from '@angular/core';
+import { Directive, ElementRef, effect, inject, model } from '@angular/core';
 import { NgxSelectComponent } from '../../components/select/select.component';
 
 let id = 0;
 
 @Directive({
     selector: '[ngxPopup]',
-    host: { '(click)': 'onClose()' },
-    exportAs: 'ngxPopup'
+    exportAs: 'ngxPopup',
+    host: {
+        'tabindex': '0',
+        '(click)': 'onClose()',
+        '(keypress)': 'onClose($event)',
+        '(toggle)': 'expanded.set($event.newState === "open")'
+    },
 })
 export class NgxPopupDirective {
     readonly element = inject<ElementRef<HTMLElement>>(ElementRef).nativeElement;
     private select = inject(NgxSelectComponent);
+    
+    readonly expanded = model(false);
+
+    private toggle$ = effect(() => {
+        const expanded = this.expanded();
+        expanded ? this.element.focus() : this.element.blur();
+    });
 
     constructor() {
         this.element.id ||= `ngx-popup-${id++}`;
-        if (this.element instanceof HTMLDialogElement) {
-            const positionAnchor = `--${this.element.id}`;
-            if (!getComputedStyle(this.element).positionAnchor) this.element.style.setProperty('position-anchor', '--' + positionAnchor);
-            if (!this.element.getAttribute('closedby')) this.element.setAttribute('closedby', 'any');
-        } else this.element.popover = 'auto';
+        const positionAnchor = `--${this.element.id}`;
+        if (!getComputedStyle(this.element).positionAnchor) this.element.style.setProperty('position-anchor', '--' + positionAnchor);
+        if (this.element instanceof HTMLDialogElement) !this.element.getAttribute('closedby') && this.element.setAttribute('closedby', 'any');
+        else this.element.popover = 'auto';
     }
 
-    onClose(): void {
-        if (!this.select.multi()) this.element instanceof HTMLDialogElement
-            ? this.element.close()
-            : this.element.hidePopover();
+    protected onClose(event?: KeyboardEvent): void {
+        if (event && event.code !== 'Enter') return;
+        if (!this.select.multi()) this.element instanceof HTMLDialogElement ? this.element.close() : this.element.hidePopover();
     }
 }
--- projects/ngx-uic/select/src/directives/trigger/trigger.directive.ts
+++ projects/ngx-uic/select/src/directives/trigger/trigger.directive.ts
@@ -1,9 +1,14 @@
-import { Directive, effect, ElementRef, inject } from '@angular/core';
+import { Directive, ElementRef, effect, inject } from '@angular/core';
 import { NgxSelectComponent } from '../../components/select/select.component';
 
 @Directive({
     selector: '[ngxTrigger]',
-    exportAs: 'ngxTrigger'
+    exportAs: 'ngxTrigger',
+    host: {
+        'tabindex': '0',
+        '(click)': 'onOpen()',
+        '(keypress)': 'onOpen($event)'
+    }
 })
 export class NgxTriggerDirective {
     readonly element = inject<ElementRef<HTMLElement>>(ElementRef).nativeElement;
@@ -11,11 +16,19 @@ export class NgxTriggerDirective {
 
     private bind$ = effect(() => {
         const popup = this.select.popup().element;
+        const positionAnchor = getComputedStyle(popup).positionAnchor;
+        if (getComputedStyle(this.element).anchorName !== positionAnchor) this.element.style.setProperty('anchor-name', positionAnchor);
         if (popup instanceof HTMLDialogElement) {
-            const positionAnchor = getComputedStyle(popup).positionAnchor;
-            if (getComputedStyle(this.element).anchorName !== positionAnchor) this.element.style.setProperty('anchor-name', positionAnchor);
             if (!this.element.getAttribute('command')) this.element.setAttribute('command', 'show-modal');
             if (!this.element.getAttribute('commandfor')) this.element.setAttribute('commandfor', popup.id);
         } else if(!this.element.getAttribute('popovertarget')) this.element.setAttribute('popovertarget', popup.id);
     });
+
+    protected onOpen(event?: KeyboardEvent): void {
+        if (event && event.code !== 'Enter') return;
+        if (this.element instanceof HTMLButtonElement) return;
+        if (this.element instanceof HTMLInputElement && this.element.type === 'button') return;
+        const popup = this.select.popup().element;
+        if (!this.select.popup().expanded()) popup instanceof HTMLDialogElement ? popup.showModal() : popup.showPopover();
+    }
 }
